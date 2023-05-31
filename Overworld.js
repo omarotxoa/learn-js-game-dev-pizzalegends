cb28fd4d247d34da50c5d39497b9f54d7ebf5bb3
Omar Ochoa <omar@ochoawebdesign.com>
2023-05-30 19:14:39 -0700

    Simple draw image to canvas

--- Overworld.js
+++ Overworld.js
@@ -6,6 +6,10 @@ class Overworld {
     }
 
     init() {
-        console.log("From Overworld.js", this);
+        const image = new Image();
+        image.onload = () => {
+            this.ctx.drawImage(image, 0, 0);
+        };
+        image.src = '/images/maps/DemoLower.png';
     }
 }
